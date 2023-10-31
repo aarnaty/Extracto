1282122302429d0ded684158ead8326d71c18a0d
aarnaty <89855782+aarnaty@users.noreply.github.com>
2023-10-31 20:12:10 +0530

    Update 2_😶_Sentiment_Analyzer.py

--- pages/2_😶_Sentiment_Analyzer.py
+++ pages/2_😶_Sentiment_Analyzer.py
@@ -3,7 +3,7 @@ import streamlit as st
 from langchain import OpenAI
 
 def analyze_sentiment(text):
-	openai_api_key="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"
+	openai_api_key="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"
 	llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
 	response=llm(f'This is a sentiment analysis request. Can you tell me the sentiment of the following text? Be elaborate.\n"{text}"\nSentiment: {{sentiment}}')
 	return response
