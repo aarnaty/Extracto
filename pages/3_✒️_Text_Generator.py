14ba9c58d707b04d3f9c72dca1715aaaacfbe75a
aarnaty <89855782+aarnaty@users.noreply.github.com>
2023-11-01 01:41:10 +0530

    Update 3_✒️_Text_Generator.py

--- pages/3_✒️_Text_Generator.py
+++ pages/3_✒️_Text_Generator.py
@@ -3,7 +3,7 @@ from langchain import OpenAI
 import os
 
 def translate(text):
-	openai_api_key=os.environ["OPENAI_API_KEY"]
+	openai_api_key=st.secrets["OPENAI_API_KEY"]
 	llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
 	response=llm(f'This is a text generation request. Can you generate a few lines for the following prompt: \n"{text}"\nGenerated Text: {{generated}}')
 	return response
